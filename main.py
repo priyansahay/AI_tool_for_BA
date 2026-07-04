7ad1020aa64ca26fdc1c71173fe95e6013d9db85
Priyanshu Sahay <priyanshusahay489@gmail.com>
2026-07-04 18:31:02 +0530

    Adding code for Exploratory Data Analysis

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.data_cleaning import cleaning_summary, clean_data, save_clean_data
 from src.data_ingestion import load_data, print_dataset_info
 from src.custom_exception import log_exception
+from src.eda import country_analysis, product_analysis, revenue_analysis,weekday_analysis,customer_analysis,monthly_sales_analysis,coorelation_analysis,categorical_summary,numerical_summary
 import logging
 import pandas as pd
 
---
+++ src/eda.py
@@ -0,0 +1,65 @@
+from src.custom_exception import log_exception
+import pandas as pd
+
+try:
+    def dataset_overview(df):
+        return {"rows": df.shape[0],
+                "column": df.shape[1],
+                "missing Values": df.isnull().sum()
+                }
+    
+    def categorical_summary(df):
+        categoricalcol = ["country","Weekday"]
+        summary = {}
+        for i in categoricalcol:
+            summary[i] = (
+                df(i).value_counts().head(10)
+            )
+        return summary
+    
+
+    def numerical_summary(df):
+        return df[
+            "Quantity", "UnitPrice", "Revenue"
+        ].describe()
+    
+    def revenue_analysis(df):
+        return{
+            "Total Revenue" : df["Revenue"].sum(),
+            "Average Revenue" : df["Revenue"].mean(),
+            "Maximum Revenue" : df["Revenue"].max()
+        }
+
+    def country_analysis(df):
+        return (
+            df.groupby("Country")["Revenue"].sum().sort_values(ascending = False)
+            )
+    
+    def customer_analysis(df):
+        return(
+            df.groupby("CustomerID")["Revenue"].sum().sort_values(ascending = False).head(10)
+        )
+    
+    def product_analysis(df):
+        return(
+            df.groupby("Description")["Revenue"].sum().sort_values(ascending = False).head(10)
+        )
+    def monthly_sales_analysis(df):
+        return(
+            df.groupby(["Year","Month"])["Revenue"].sum().sort_values(ascending = False).head(10)
+        )
+    
+    def weekday_analysis(df):
+        return (
+            df.groupby("Weekday")["Revenue"].sum()
+        )
+    
+    def coorelation_analysis(df):
+        cols = ["Quantity", "UnitPrice","Revenue" ]
+        return df[cols].corr()
+    
+    
+except Exception as e:
+    log_exception(e)
+    raise
+    

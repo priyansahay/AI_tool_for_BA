2219af93aec6cbe65abec41b3f67fe98e3ec1ba1
Priyanshu Sahay <priyanshusahay489@gmail.com>
2026-07-04 14:57:10 +0530

    Adding Exception Handler

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
-from src.data_cleaning import cleaning_summary, clean_data
-from src.data_ingestion import load_data
+from src.data_cleaning import cleaning_summary, clean_data, save_clean_data
+from src.data_ingestion import load_data, print_dataset_info
+from src.custom_exception import log_exception
+import logging
+import pandas as pd
 
+raw_data_path = r"data/raw/Online Retail.xlsx"
+clean_data_path = r"data/processed/Clean File.csv"
+
+def main():
+    try:
+        # DATA INGESTION
+        logging.info("Pipeline Started\n")
+        df = load_data(raw_data_path)
+        print_dataset_info(df)
+
+        # CLEANING DATA
+        logging.info("Data Cleaning\n")
+        cleaned_df = clean_data(df)
+        save_clean_data(cleaned_df,clean_data_path)
+        original_df = pd.read_excel(raw_data_path)
+        cleaning_summary(original_df,cleaned_df)
+
+        logging.info("Cleaned Data set successfilly")
+        print(f"Final Shape: {cleaned_df.shape}")
+
+    except Exception as e:
+        log_exception(e)
+        raise
+
+if __name__ == "__main__":
+    main()
--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,95 +1,102 @@
 import pandas as pd
 import logging
+from src.custom_exception import log_exception
 
-logger = logging.getLogger(__name__)
+try:
+    logger = logging.getLogger(__name__)
 
-def check_missing_values(df):
-    return df.isnull().sum()
+    def check_missing_values(df):
+        return df.isnull().sum()
 
-def remove_duplicates(df):
-    return df.isnull().sum()
+    def remove_duplicates(df):
+        return df.isnull().sum()
 
-def remove_duplicates(df):
-    initial_rows = len(df)
-    df = df.drop_duplicates()
-    logger.info(
-        f"Removed {initial_rows - len(df)} duplicates rows."
-    )
-    return df
+    def remove_duplicates(df):
+        initial_rows = len(df)
+        df = df.drop_duplicates()
+        logger.info(
+            f"Removed {initial_rows - len(df)} duplicates rows."
+        )
+        return df
 
-def convert_date_column(df):
-    df["InvoiceDate"] = pd.to_datetime(df["InvoiceDate"])
-    return df
+    def convert_date_column(df):
+        df["InvoiceDate"] = pd.to_datetime(df["InvoiceDate"])
+        return df
 
-def remove_missing_customer_id(df):
-    initialrows = len(df)
-    df=df.dropna(subset = ["CustomerID"]) 
-    logger.info(f"{initialrows - len(df)} rows with missing Customer IDs")
-    return df
+    def remove_missing_customer_id(df):
+        initialrows = len(df)
+        df=df.dropna(subset = ["CustomerID"]) 
+        logger.info(f"{initialrows - len(df)} rows with missing Customer IDs")
+        return df
 
-def remove_missing_description(df):
-    initialrows = len(df)
-    df=df.dropna(subset = ["Description"]) 
-    logger.info(f"Deleted {initialrows - len(df)} rows with missing Description")
-    return df
+    def remove_missing_description(df):
+        initialrows = len(df)
+        df=df.dropna(subset = ["Description"]) 
+        logger.info(f"Deleted {initialrows - len(df)} rows with missing Description")
+        return df
 
-def removed_cancel_orders(df):
-    initial_rows = len(df)
-    df=df[~df["InvoiceNo"].astype(str).str.startswith("C")]
-    logger.info(f"Deleted {initial_rows - len(df)} rows with CANCELLED Invoice")
-    return df
+    def removed_cancel_orders(df):
+        initial_rows = len(df)
+        df=df[~df["InvoiceNo"].astype(str).str.startswith("C")]
+        logger.info(f"Deleted {initial_rows - len(df)} rows with CANCELLED Invoice")
+        return df
 
-def remove_negative_quant(df):
-    initial_rows = len(df)
-    df=df[df["Quantity"]>0]
-    logger.info(f"Deleted {initial_rows - len(df)} rows with negative Quantity")
-    return df
+    def remove_negative_quant(df):
+        initial_rows = len(df)
+        df=df[df["Quantity"]>0]
+        logger.info(f"Deleted {initial_rows - len(df)} rows with negative Quantity")
+        return df
 
-def removed_invalid_price(df):
-    initialrows = len(df)
-    df= df[df["UnitPrice"]>0]
-    logger.info(f"Deleted {initialrows - len(df)} rows with Invalid price records.")
-    return df
+    def removed_invalid_price(df):
+        initialrows = len(df)
+        df= df[df["UnitPrice"]>0]
+        logger.info(f"Deleted {initialrows - len(df)} rows with Invalid price records.")
+        return df
 
-def standardize_ID(df):
-    df["CustomerID"] = (df["CustomerID"].astype(int).astype(str))
-    return df
+    def standardize_ID(df):
+        df["CustomerID"] = (df["CustomerID"].astype(int).astype(str))
+        return df
 
-def revenue_column(df):
-    df["Revenue"]=(df["Quantity"]*df["Unitprice"])
-    return df
+    def revenue_column(df):
+        df["Revenue"]=(df["Quantity"]*df["UnitPrice"])
+        return df
 
-def create_date(df):
-    df["Year"] = df["InvoiceDate"].dt.year
-    df["Month"] = df["InvoiceDate"].dt.month
-    df["Quarter"] = df["InvoiceDate"].dt.quarter
-    df["Day"] = df["InvoiceDate"].dt.day
-    df["Weekday"] = df["InvoiceDate"].dt.day_name()
-    return df
+    def create_date(df):
+        df["Year"] = df["InvoiceDate"].dt.year
+        df["Month"] = df["InvoiceDate"].dt.month
+        df["Quarter"] = df["InvoiceDate"].dt.quarter
+        df["Day"] = df["InvoiceDate"].dt.day
+        df["Weekday"] = df["InvoiceDate"].dt.day_name()
+        return df
 
-def save_clean_data(df, outputpath):
-    df.to_csv(outputpath, index = False)
-    logger.info(f"Cleaned dataset saved to {outputpath}")
+    def save_clean_data(df, outputpath):
+        df.to_csv(outputpath, index = False)
+        logger.info(f"Cleaned dataset saved to {outputpath}")
+        return df
 
-def cleaning_summary(original_df, cleaned_df):
-    return {
-        "original_rows" : len(original_df),
-        "cleaned_df" : len(cleaned_df),
-        "rows_removed" : len(original_df) - len (cleaned_df)
-    }
+    def cleaning_summary(original_df, cleaned_df):
+        return {
+            "original_rows" : len(original_df),
+            "cleaned_df" : len(cleaned_df),
+            "rows_removed" : len(original_df) - len (cleaned_df)
+        }
 
-def clean_data(df):
-    logger.info("Started Cleaning Data...\n")
-    df = remove_duplicates(df)
-    df = convert_date_column(df)
-    df = remove_missing_customer_id(df)
-    df = remove_missing_description(df)
-    df = removed_cancel_orders(df)
-    df = remove_negative_quant(df)
-    df = removed_invalid_price(df)
-    df = standardize_ID(df)
-    df = revenue_column(df)
-    df = create_date(df)
-    logger.info("Data Cleaning completed...\n")
+    def clean_data(df):
+        logger.info("Started Cleaning Data...\n")
+        df = remove_duplicates(df)
+        df = convert_date_column(df)
+        df = remove_missing_customer_id(df)
+        df = remove_missing_description(df)
+        df = removed_cancel_orders(df)
+        df = remove_negative_quant(df)
+        df = removed_invalid_price(df)
+        df = standardize_ID(df)
+        df = revenue_column(df)
+        df = create_date(df)
+        logger.info("Data Cleaning completed...\n")
+        return df
 
+except Exception as e:
+    log_exception(e)
+    raise
 
